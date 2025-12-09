d1b07861879df45295e1a2451b5676b34ada41a2
quantumprojects15432-collab <quantumporjects15432@gmail.com>
2025-12-09 15:38:13 +0500

    change the image size

--- src/components/UI/DrinkItem.jsx
+++ src/components/UI/DrinkItem.jsx
@@ -10,7 +10,7 @@ const CarItem = (props) => {
     <Col lg="4" md="4" sm="6" className="mb-5">
       <div className="car__item">
         <div className="car__img">
-          <img src={imgUrl} alt="" className="w-100" />
+          <img src={imgUrl} alt="" className="w-100"  style={{ height: "450px", objectFit: "cover" }}/>
         </div>
 
         <div className="car__item-content mt-4">
